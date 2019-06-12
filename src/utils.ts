44e6e96b61a794be1df9a0460add60580ec1293e
Zura Gabievi <zura.gabievi@gmail.com>
2019-06-13 02:02:30 +0400

    json stringify without circular error

--- src/utils.ts
+++ src/utils.ts
@@ -40,3 +40,47 @@ export function safeJsonParse(str: string): { [key: string]: any } | string {
     return str;
   }
 }
+
+export function serializer(
+  replacer: Function = null,
+  cycleReplacer: Function = null
+): any {
+  const stack = [];
+  const keys = [];
+
+  if (cycleReplacer == null) {
+    cycleReplacer = function(_, value: any) {
+      if (stack[0] === value) {
+        return '[Circular ~]';
+      }
+
+      return (
+        '[Circular ~.' + keys.slice(0, stack.indexOf(value)).join('.') + ']'
+      );
+    };
+  }
+
+  return function(key: string, value: any) {
+    if (stack.length > 0) {
+      const thisPos = stack.indexOf(this);
+      ~thisPos ? stack.splice(thisPos + 1) : stack.push(this);
+      ~thisPos ? keys.splice(thisPos, Infinity, key) : keys.push(key);
+      if (~stack.indexOf(value)) {
+        value = cycleReplacer.call(this, key, value);
+      }
+    } else {
+      stack.push(value);
+    }
+
+    return replacer == null ? value : replacer.call(this, key, value);
+  };
+}
+
+export function safeJsonStringify(
+  obj: any,
+  replacer: Function = null,
+  spaces?: number,
+  cycleReplacer: Function = null
+): string {
+  return JSON.stringify(obj, serializer(replacer, cycleReplacer), spaces);
+}
